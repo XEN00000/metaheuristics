e06f4b6081473a58bcb658b27164fa1f07dd2857
blanqtoja <251544@edu.p.lodz.pl>
2025-11-02 12:58:39 +0100

    function from section 3 example 1

---
+++ simulated_annealing/demo_functions.py
@@ -0,0 +1,8 @@
+class section3:
+    def ex1(x):
+        if -105 <= x <= -95:
+            return -2 * abs(x + 100) + 10
+        if 95 < x <= 105:
+            return -2 * abs(x + 100) + 10
+
+        return 0
